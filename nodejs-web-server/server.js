3fcb4516edbd36a869aaf5afcc44c69b9e55647c
Tryo Asnafi <tryoasnafi@gmail.com>
2021-05-15 17:30:39 +0700

    Add routing request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -4,34 +4,41 @@ const host = "localhost";
 const port = 5000;
 
 const requestListener = (request, response) => {
-  const { method } = request;
+  response.setHeader("Content-Type", "text/html");
+  response.statusCode = 200;
 
-  response.writeHeader(200, "Content-Type", "text/html");
+  const { method, url } = request;
 
-  if (method === "GET") {
-    response.end("<h1>Hello!</h1>");
-  }
+  if (url === "/") {
+    if (method === "GET") {
+      response.end("<h1>Ini adalah homepage!</h1>");
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else if (url === "/about") {
+    if (method === "GET") {
+      response.end("<h1>Hallo! Ini adalah halaman about</h1>");
+    } else if (method === "POST") {
+      let body = [];
 
-  if (method === "POST") {
-    let body = [];
+      request.on("data", (chunk) => {
+        body.push(chunk);
+      });
 
-    request.on("data", (chunk) => {
-      body.push(chunk);
-    });
-
-    request.on("end", () => {
-      body = Buffer.concat(body).toString();
-      const { name } = JSON.parse(body);
-      response.end(`<h1>Hai, ${name}!</h1>`);
-    });
-  }
-
-  if (method === "PUT") {
-    response.end("<h1>Bonjour!</h1>");
-  }
-
-  if (method === "DELETE") {
-    response.end("<h1>Salam!</h1>");
+      request.on("end", () => {
+        body = Buffer.concat(body).toString();
+        const { name } = JSON.parse(body);
+        response.end(`<h1>Hallo ${name}! Ini adalah halaman about</h1>`);
+      });
+    } else {
+      response.end(
+        `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
+      );
+    }
+  } else {
+    response.end("<h1>Halaman tidak ditemukan</h1>");
   }
 };
 
