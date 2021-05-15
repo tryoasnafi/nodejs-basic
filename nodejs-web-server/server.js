369984f64ab4692df721f3f66e2e11f68254a665
Tryo Asnafi <tryoasnafi@gmail.com>
2021-05-15 17:40:17 +0700

    Add custom status code on every route

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -5,20 +5,22 @@ const port = 5000;
 
 const requestListener = (request, response) => {
   response.setHeader("Content-Type", "text/html");
-  response.statusCode = 200;
 
   const { method, url } = request;
 
   if (url === "/") {
     if (method === "GET") {
+      response.statusCode = 200;
       response.end("<h1>Ini adalah homepage!</h1>");
     } else {
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
       );
     }
   } else if (url === "/about") {
     if (method === "GET") {
+      response.statusCode = 200;
       response.end("<h1>Hallo! Ini adalah halaman about</h1>");
     } else if (method === "POST") {
       let body = [];
@@ -30,14 +32,17 @@ const requestListener = (request, response) => {
       request.on("end", () => {
         body = Buffer.concat(body).toString();
         const { name } = JSON.parse(body);
+        response.statusCode = 200;
         response.end(`<h1>Hallo ${name}! Ini adalah halaman about</h1>`);
       });
     } else {
+      response.statusCode = 400;
       response.end(
         `<h1>Halaman tidak dapat diakses dengan ${method} request</h1>`
       );
     }
   } else {
+    response.statusCode = 404;
     response.end("<h1>Halaman tidak ditemukan</h1>");
   }
 };
