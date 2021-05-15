0b385e39f462fd8fb725acc93fa942a2eb6d6a75
Tryo Asnafi <tryoasnafi@gmail.com>
2021-05-15 16:47:01 +0700

    Add body request

--- nodejs-web-server/server.js
+++ nodejs-web-server/server.js
@@ -13,7 +13,17 @@ const requestListener = (request, response) => {
   }
 
   if (method === "POST") {
-    response.end("<h1>Hai!</h2>");
+    let body = [];
+
+    request.on("data", (chunk) => {
+      body.push(chunk);
+    });
+
+    request.on("end", () => {
+      body = Buffer.concat(body).toString();
+      const { name } = JSON.parse(body);
+      response.end(`<h1>Hai, ${name}!</h1>`);
+    });
   }
 
   if (method === "PUT") {
